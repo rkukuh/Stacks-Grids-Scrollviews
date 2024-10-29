aded059e328aa0801c2d2842096f8797c44c8840
R. Kukuh <rkukuh@gmail.com>
2024-10-29 14:47:05 +0700

    Initial Commit

---
+++ Stacks, Grids, Scrollviews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stacks, Grids, Scrollviews
+//
+//  Created by R. Kukuh on 29/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stacks, Grids, Scrollviews/Stacks__Grids__ScrollviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stacks__Grids__ScrollviewsApp.swift
+//  Stacks, Grids, Scrollviews
+//
+//  Created by R. Kukuh on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Stacks__Grids__ScrollviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
