84bd4acae3ac7e2a73067d92a9127952ede89264
R. Kukuh <rkukuh@gmail.com>
2024-10-29 14:53:30 +0700

    03

--- Stacks, Grids, Scrollviews/ContentView.swift
+++ Stacks, Grids, Scrollviews/ContentView.swift
@@ -9,11 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("SwiftUI")
-            
-            Divider()
-            
             Text("rocks")
         }
     }
